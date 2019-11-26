78a0143e22297f5e2a852d39c4e08e0518b9fe24
rwe <rwe@computer.lan>
2019-11-26 16:10:02 +0100

    daco som nakodoval

--- src/index/index.ts
+++ src/index/index.ts
@@ -5,13 +5,14 @@ import {buildElement} from '../utils/html';
 async function main() {
   console.log('hello sd game!');
 
-  const buttonNode = buildElement('button', {
+  const buttonNode = buildElement('div', {
     textContent: 'Click me!',
     handlers: {
       onclick: () => buttonNode.textContent = 'Clicked!',
       onmouseover: () => buttonNode.style.width = '200px',
     },
   });
+
 
   document.body.appendChild(buttonNode);
 
